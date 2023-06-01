ba4e59d29a9c4e4fe63d11a042a919e2577ce951
Nishtha1012 <104210575+Nishtha1012@users.noreply.github.com>
2023-06-01 17:41:03 +0530

    Update useSignup.js

--- src/components/hooks/useSignup.js
+++ src/components/hooks/useSignup.js
@@ -11,34 +11,30 @@ import { clearError, clearSigned, clearUser } from "../store/slice/userSlice"
 const useSignup = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const { isSigned } = useSelector(state => state.user)
+    const { isSigned, error } = useSelector((state) => state.user)
+
+  useEffect(() => {
+    if (isSigned === true) {
+      navigate(path.loginPage)
+      alert('user registered')
+      dispatch(clearSigned())
+    }
+  }, [isSigned])
+
+  useEffect(() => {
+    if (error) {
+      navigate(path.signupPage)
+      alert('user already existss')
+      dispatch(clearError())
+    }
+  }, [error])
+
 
     //adds the new user in database
     //shows alert of user signup succesfully
     const signUpUser = (data) => {
         const result = dispatch(createUserAction(data))
         console.log("issigned", isSigned);
-        if (isSigned === -1) {
-            Swal.fire({
-                position: 'top-end',
-                icon: 'error',
-                title: 'User already exist',
-                showConfirmButton: false,
-                timer: 1500
-            })
-            dispatch(clearError())
-        }
-        else {
-            Swal.fire({
-                position: 'top-end',
-                icon: 'success',
-                title: 'User Registered',
-                showConfirmButton: false,
-                timer: 1500
-            })
-            navigate(path.loginPage)
-            dispatch(clearSigned())
-        }
     }
     return { signUpUser }
 }
